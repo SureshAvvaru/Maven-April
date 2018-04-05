6033644b013cb14b4c476815ade5ff5aa77629a7
SureshAvvaru <suresh.avvaru123@gmail.com>
2018-04-05 19:21:33 +0530

    Update App.java

--- mavenTestApp/src/main/java/org/mavenproject/App.java
+++ mavenTestApp/src/main/java/org/mavenproject/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "WELCOME TO FIRST MAVEN PROJECT!" );
+        System.out.println( "WELCOME TO FIRST MAVEN PROJECTS!" );
     }
 }
